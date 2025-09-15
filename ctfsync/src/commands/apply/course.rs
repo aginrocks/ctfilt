9e3f84d5303b888e1ba3ff7aae9d56c433f4c26b
Tymek <vonautymek@gmail.com>
2025-09-16 00:42:48 +0200

    ctfsync: fixes

--- ctfsync/src/commands/apply/course.rs
+++ ctfsync/src/commands/apply/course.rs
@@ -2,10 +2,9 @@ use std::path::Path;
 
 use api_client::{
     apis::courses_api,
-    models::{self, UpdateCourseRequest},
+    models::{CourseMetadata, UpdateCourseRequest},
 };
 use gix::Repository;
-use manifests::CourseMetadata;
 use miette::{IntoDiagnostic, Result};
 
 use crate::{api::init_api_config, errors::NoManifest};
@@ -18,35 +17,25 @@ pub async fn run(repo: Repository, directory: &Path) -> Result<()> {
         .id
         .to_string();
 
-    // Loading manifest
+    let manifest = load_manifest(directory).await?;
+
+    let config = init_api_config().await?;
+
+    let update_request = UpdateCourseRequest::new(manifest.clone(), r#ref);
+    let update_response = courses_api::update_course(config, &manifest.slug, update_request)
+        .await
+        .into_diagnostic()?;
+
+    dbg!(update_response);
+
+    Ok(())
+}
+
+async fn load_manifest(directory: &Path) -> Result<CourseMetadata> {
     let manifest = directory.join("course.yaml");
     let manifest = tokio::fs::read_to_string(manifest)
         .await
         .map_err(|_| NoManifest)?;
     let manifest = serde_yaml::from_str::<CourseMetadata>(&manifest).into_diagnostic()?;
-
-    let config = init_api_config().await?;
-
-    let update_request = UpdateCourseRequest::new(
-        models::CourseMetadata {
-            description: manifest.description,
-            difficulty: match manifest.difficulty {
-                manifests::CourseDifficulty::Advanced => models::CourseDifficulty::Advanced,
-                manifests::CourseDifficulty::Beginner => models::CourseDifficulty::Beginner,
-                manifests::CourseDifficulty::Intermediate => models::CourseDifficulty::Intermediate,
-            },
-            name: manifest.name,
-            objectives: Some(manifest.objectives),
-            prerequisites: Some(manifest.prerequisites),
-            slug: manifest.slug.clone(),
-            tags: Some(manifest.tags),
-        },
-        r#ref,
-    );
-    let update_response = courses_api::update_course(config, &manifest.slug, update_request)
-        .await
-        .into_diagnostic()?;
-    dbg!(update_response);
-
-    Ok(())
+    Ok(manifest)
 }
