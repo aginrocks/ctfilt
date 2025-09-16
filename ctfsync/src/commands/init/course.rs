88d962b01961ce3e8e0c91be186a76943aeb8ade
Tymek <vonautymek@gmail.com>
2025-09-16 08:57:21 +0200

    ctfsync: added manifst init for courses

--- ctfsync/src/commands/init/course.rs
+++ ctfsync/src/commands/init/course.rs
@@ -1,9 +1,16 @@
+use std::path::Path;
+
 use clap::Parser;
-use inquire::Text;
-use manifests::{CourseDifficulty, to_slug};
-use miette::{IntoDiagnostic, Result};
+use gix::ThreadSafeRepository;
+use inquire::{Select, Text};
+use manifests::{CourseDifficulty, CourseMetadata, serialize_with_schema, to_slug};
+use miette::{IntoDiagnostic, Result, miette};
 use strum::IntoEnumIterator;
-use tokio::task;
+use tokio::{fs, task};
+use tracing::warn;
+use url::Url;
+
+use crate::{config::init_config, errors::NoGitWorkdir};
 
 #[derive(Debug, Clone, Parser)]
 pub struct InitCourseArgs {
@@ -50,8 +57,46 @@ pub async fn run(args: InitCourseArgs) -> Result<()> {
 
     let options = CourseDifficulty::iter().collect::<Vec<_>>();
 
-    // let diffic
+    let difficulty = match args.difficulty {
+        Some(difficulty) => difficulty,
+        None => task::spawn_blocking(move || {
+            Select::new("Difficulty", options)
+                .with_starting_cursor(0)
+                .prompt()
+        })
+        .await
+        .into_diagnostic()?
+        .into_diagnostic()?,
+    };
 
-    dbg!(options);
+    let repo = ThreadSafeRepository::discover(".");
+    let repo = match repo {
+        Ok(repo) => repo.into(),
+        Err(_) => {
+            warn!("Git repository not found, initializing one");
+            gix::init(Path::new(".")).into_diagnostic()?
+        }
+    };
+
+    let directory = repo.workdir().ok_or(NoGitWorkdir)?;
+    let manifest_path = directory.join("course.yaml");
+
+    let metadata = CourseMetadata {
+        name,
+        slug,
+        description,
+        difficulty,
+        ..Default::default()
+    };
+
+    let config = init_config().await?;
+    let server_base = Url::parse(&config.base_url).into_diagnostic()?;
+    let serialized_manifest = serialize_with_schema(metadata, "course", server_base)
+        .map_err(|_| miette!("Failed to serialize manifest"))?;
+
+    fs::write(manifest_path, serialized_manifest)
+        .await
+        .into_diagnostic()?;
+
     Ok(())
 }
