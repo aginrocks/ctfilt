a55eb82a39bc56ffeed4d91c709a2869e32200ae
Tymek <vonautymek@gmail.com>
2025-09-17 21:26:30 +0200

    api_client: generated

--- api_client/docs/CourseApi.md
+++ api_client/docs/CourseApi.md
@@ -6,7 +6,6 @@ Method | HTTP request | Description
 ------------- | ------------- | -------------
 [**get_course_lesson**](CourseApi.md#get_course_lesson) | **GET** /api/courses/{course_slug}/lessons/{lesson_slug} | Get lesson
 [**get_course_lessons**](CourseApi.md#get_course_lessons) | **GET** /api/courses/{course_slug}/lessons | Get lessons
-[**update_course_lesson**](CourseApi.md#update_course_lesson) | **PUT** /api/courses/{course_slug}/lessons/{lesson_slug} | Update lesson
 
 
 
@@ -62,38 +61,6 @@ No authorization required
 ### HTTP request headers
 
 - **Content-Type**: Not defined
-- **Accept**: application/json
-
-[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
-
-
-## update_course_lesson
-
-> models::CreateSuccess update_course_lesson(course_slug, lesson_slug, update_lesson_request)
-Update lesson
-
-If lesson is not found, it will be created
-
-### Parameters
-
-
-Name | Type | Description  | Required | Notes
-------------- | ------------- | ------------- | ------------- | -------------
-**course_slug** | **String** | Course slug | [required] |
-**lesson_slug** | **String** | Lesson slug | [required] |
-**update_lesson_request** | [**UpdateLessonRequest**](UpdateLessonRequest.md) |  | [required] |
-
-### Return type
-
-[**models::CreateSuccess**](CreateSuccess.md)
-
-### Authorization
-
-No authorization required
-
-### HTTP request headers
-
-- **Content-Type**: application/json
 - **Accept**: application/json
 
 [[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
--- api_client/src/apis/course_api.rs
+++ api_client/src/apis/course_api.rs
@@ -33,14 +33,6 @@ pub enum GetCourseLessonsError {
     UnknownValue(serde_json::Value),
 }
 
-/// struct for typed errors of method [`update_course_lesson`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UpdateCourseLessonError {
-    Status401(models::UnauthorizedError),
-    UnknownValue(serde_json::Value),
-}
-
 
 pub async fn get_course_lesson(configuration: &configuration::Configuration, course_slug: &str, lesson_slug: &str) -> Result<Vec<models::LessonMetadata>, Error<GetCourseLessonError>> {
     // add a prefix to parameters to efficiently prevent name collisions
@@ -111,46 +103,6 @@ pub async fn get_course_lessons(configuration: &configuration::Configuration, co
     } else {
         let content = resp.text().await?;
         let entity: Option<GetCourseLessonsError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent { status, content, entity }))
-    }
-}
-
-/// If lesson is not found, it will be created
-pub async fn update_course_lesson(configuration: &configuration::Configuration, course_slug: &str, lesson_slug: &str, update_lesson_request: models::UpdateLessonRequest) -> Result<models::CreateSuccess, Error<UpdateCourseLessonError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_path_course_slug = course_slug;
-    let p_path_lesson_slug = lesson_slug;
-    let p_body_update_lesson_request = update_lesson_request;
-
-    let uri_str = format!("{}/api/courses/{course_slug}/lessons/{lesson_slug}", configuration.base_path, course_slug=crate::apis::urlencode(p_path_course_slug), lesson_slug=crate::apis::urlencode(p_path_lesson_slug));
-    let mut req_builder = configuration.client.request(reqwest::Method::PUT, &uri_str);
-
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    req_builder = req_builder.json(&p_body_update_lesson_request);
-
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => return Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::CreateSuccess`"))),
-            ContentType::Unsupported(unknown_type) => return Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::CreateSuccess`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UpdateCourseLessonError> = serde_json::from_str(&content).ok();
         Err(Error::ResponseError(ResponseContent { status, content, entity }))
     }
 }
