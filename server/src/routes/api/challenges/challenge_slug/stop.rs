e103e81f6c708e19032a58fc9f13a61b6bb1bb90
Tymek <vonautymek@gmail.com>
2025-09-28 04:32:32 +0200

    server: added challenge stopping

--- server/src/routes/api/challenges/challenge_slug/stop.rs
+++ server/src/routes/api/challenges/challenge_slug/stop.rs
@@ -1,13 +1,15 @@
-use axum::Json;
+use axum::{Extension, Json, extract::Path};
+use color_eyre::eyre::{ContextCompat, eyre};
 use utoipa_axum::routes;
 
 use crate::{
-    axum_error::AxumResult,
-    middlewares::require_auth::UnauthorizedError,
+    axum_error::{AxumError, AxumResult},
+    middlewares::require_auth::{UnauthorizedError, UserId},
     routes::{
         RouteProtectionLevel,
         api::{NotFoundError, challenges::challenge_slug::KubernetesActionResult},
     },
+    state::AppState,
 };
 
 use super::Route;
@@ -36,6 +38,32 @@ pub fn routes() -> Vec<Route> {
     ),
     tag = "Challenge"
 )]
-async fn stop_challenge() -> AxumResult<Json<KubernetesActionResult>> {
-    todo!()
+async fn stop_challenge(
+    Extension(state): Extension<AppState>,
+    Extension(user_id): Extension<UserId>,
+    Path(challenge_slug): Path<String>,
+) -> AxumResult<Json<KubernetesActionResult>> {
+    let challenge = state.store.challenges.get_by_slug(&challenge_slug).await?;
+
+    let pod_name = state
+        .orchestrator
+        .watcher
+        .users_state
+        .get(&user_id)
+        .ok_or_else(|| AxumError::not_found(eyre!("Challenge is not running")))?
+        .value()
+        .challenges
+        .get(&challenge.id)
+        .ok_or_else(|| AxumError::not_found(eyre!("Challenge is not running")))?
+        .hostname
+        .clone()
+        .wrap_err("Missing challenge hostname")?;
+
+    state
+        .orchestrator
+        .exterminator
+        .exterminate(pod_name)
+        .await?;
+
+    Ok(Json(KubernetesActionResult { success: true }))
 }
