cfb4c7ca5c6df9e15986964cd2829d5896334665
Tymek <vonautymek@gmail.com>
2025-10-08 23:29:14 +0200

    web: small refactor

--- web/components/running-challenge.tsx
+++ web/components/running-challenge.tsx
@@ -19,16 +19,8 @@ function ChallengeLabel({ icon: Icon, label }: ChallengeLabelProps) {
     );
 }
 
-export function RunningChallengeBox({
-    hostname,
-    expires_at,
-    status,
-    name,
-    slug,
-    _id,
-    ip,
-}: RunningChallenge) {
-    const countdown = useCountdown(expires_at);
+export function RunningChallengeBox(challenge: RunningChallenge) {
+    const countdown = useCountdown(challenge.expires_at);
     const canExtend = countdown.seconds <= EXTEND_TRESHOLD_SECONDS;
 
     const modals = useModals();
@@ -38,7 +30,7 @@ export function RunningChallengeBox({
             className="p-2 pt-2.5 border rounded-lg cursor-pointer hover:bg-secondary/30 transition-colors"
             onClick={() =>
                 modals.show('Challenge', {
-                    slug,
+                    slug: challenge.slug,
                 })
             }
         >
@@ -46,26 +38,18 @@ export function RunningChallengeBox({
                 <IconBox className="size-3.5" />
                 <p className="text-xs font-medium">Challenge</p>
             </div>*/}
-            <p className="font-medium text-sm mb-1.5">{name}</p>
+            <p className="font-medium text-sm mb-1.5">{challenge.name}</p>
             <div className="flex flex-col gap-1">
                 <ChallengeLabel icon={IconStopwatch} label={countdown.formatted} />
-                {status === 'running' && (
-                    <ChallengeLabel icon={IconWorld} label={hostname ?? 'Unknown'} />
+                {challenge.status === 'running' && (
+                    <ChallengeLabel icon={IconWorld} label={challenge.hostname ?? 'Unknown'} />
                 )}
                 <div className="flex gap-2 mt-1.5 w-full flex-1">
                     <ChallengeActions
                         canExtend={canExtend}
-                        slug={slug}
+                        slug={challenge.slug}
                         variant="compact"
-                        running={{
-                            hostname,
-                            expires_at,
-                            status,
-                            name,
-                            slug,
-                            _id,
-                            ip,
-                        }}
+                        running={challenge}
                     />
                 </div>
             </div>
